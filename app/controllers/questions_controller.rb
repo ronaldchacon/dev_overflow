02df9fdf623028188c6cc698b1bc0faaebd88a0c
Neil Lambert & Ronald Chacon <pair+neilcode.ronaldchacon@devbootcamp.com>
2014-12-09 11:18:22 -0800

    complete release 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,45 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @answer = Answer.create(title: "new answer", content: "place content here")
     @answers = @question.answers
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+    redirect_to questions_path
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :content)
+    end
+
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
-
   validates :title, presence: true
   validates :content, presence: true
 end

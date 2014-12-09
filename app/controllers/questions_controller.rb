9d6d0b05537952df2dc4610df4d9c284c1859c4e
Jarred <jarredhare@gmail.com>
2014-12-09 08:49:45 -0800

    Add for to add questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,16 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @answers = @question.answers
   end
+
+  def create
+    @question = Question.new(question_params)
+
+    @question.save
+    redirect_to @question
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
 end
